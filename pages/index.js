af59d577439f46d7dd23f029861cae843723a39d
domobritton <domonicbritton@gmail.com>
2018-11-24 00:50:32 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const Index = () => (
+        <Layout>
+            <p>Hello Next.js</p>
+        </Layout>
+
+)
+
+export default Index
